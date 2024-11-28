ede931d9a67ef3b76f73381ea213a098e5e83328
Soonmumu <chsohee1537@gmail.com>
2024-11-28 15:37:09 +0900

    11/28

--- jhs_project1.sql
+++ jhs_project1.sql
@@ -1,3 +1,21 @@
+create table parking_reduction (
+    r_nredu number(3, 0),
+    r_name  varchar2(20 char),
+    r_ratio number(3, 1),
+    
+    constraint pk_c_nredu primary key (r_nredu),
+    constraint ck_r_name check (r_name is not null),
+    constraint ck_r_ratio check (r_ratio is not null)
+);
+
+insert into parking_reduction values (1, '해당없음', 1);
+insert into parking_reduction values (2, '경차 - 50%', 0.5);
+insert into parking_reduction values (3, '다자녀 2인 - 30%', 0.7);
+insert into parking_reduction values (4, '다자녀 3인 - 50%', 0.5);
+insert into parking_reduction values (5, '저공해 1,2종 - 50% ', 0.5);
+insert into parking_reduction values (6, '저공해 3종 - 30%', 0.7);
+insert into parking_reduction values (7, '긴급/군 차량 - 면제', 0);
+
 create table parking_info (
     c_num   varchar2(30 char),
     c_entra number(38,0),
@@ -7,29 +25,18 @@ create table parking_info (
     c_pfee  number(10, 0),
     
     constraint pk_c_num primary key (c_num),
-    constraint nn_c_nredu check (c_nredu is not null)
+    constraint fk_c_nredu foreign key (c_nredu) references parking_reduction (r_nredu)
 );
 
 create table parking_area (
     p_area  varchar2(10 char),
-    p_check number(2,0),
+    p_check number(2,0) default 0,
     c_num   varchar2(30 char),
     
     constraint pk_p_area primary key (p_area),
     constraint ck_p_check check (p_check between 0 and 1),
     constraint fk_c_num foreign key (c_num) references parking_info (c_num)
 );
-
-create table parking_reduction (
-    c_nredu number(3, 0),
-    r_name  varchar2(20 char),
-    r_ratio number(3, 1),
-    
-    constraint fk_c_nredu primary key (c_nredu),
-    constraint pk_r_rname check (r_name is not null),
-    constraint ck_r_ratio check (r_ratio is not null)
-);
-
 
 insert into parking_area (p_area) values ('1f01');
 insert into parking_area (p_area) values ('1f02');
@@ -44,17 +51,24 @@ insert into parking_area (p_area) values ('1f10');
 insert into parking_area (p_area) values ('1f11');
 insert into parking_area (p_area) values ('1f12');
 
+create table parking_finish_data (
+    f_num   varchar2(30 char),
+    f_ptime number(38, 0),
+    f_pfee  number(10, 0),
+    f_nredu number(3,0),
+    
+    constraint ch_f_num check (f_num is not null),
+    constraint ch_f_ptime check (f_ptime is not null),
+    constraint ch_f_pfee check (f_pfee is not null),
+    constraint ch_f_nredu check (f_nredu is not null)
+);
 
-insert into parking_reduction values (1, '해당없음', 1);
-insert into parking_reduction values (2, '경차 - 50%', 0.5);
-insert into parking_reduction values (3, '다자녀 2인 - 30%', 0.7);
-insert into parking_reduction values (4, '다자녀 3인 - 50%', 0.5);
-insert into parking_reduction values (5, '저공해 1,2종 - 50% ', 0.5);
-insert into parking_reduction values (6, '저공해 3종 - 30%', 0.7);
-insert into parking_reduction values (7, '긴급/군 차량 - 면제', 0);
+commit;
 
 /*
 drop table parking_area;
-drop table parking_reduction;
 drop table parking_info;
+drop table parking_reduction;
+drop table parking_finish_data;
 */
+
